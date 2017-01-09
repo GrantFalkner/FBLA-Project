311180d2058b61e4c91327109d40f41dc541c51d
Grant Falkner <grant@ardith.org>
2017-01-08 17:45:18 -0700

    upload works

---
+++ FBLA Mobile Application/DonateViewController.swift
@@ -0,0 +1,114 @@
+//
+//  DonateViewController.swift
+//  
+//
+//  Created by Grant Falkner on 1/8/17.
+//
+//
+
+import UIKit
+import Firebase
+import FirebaseStorage
+
+class DonateViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let picker = UIImagePickerController()
+    var StorageRef: FIRStorageReference!
+    var ref: FIRDatabaseReference!
+    
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var ItemName: UITextField!
+    @IBOutlet weak var ItemPrice: UITextField!
+    @IBOutlet weak var Slider: UISlider!
+    @IBOutlet weak var SliderLabel: UILabel!
+    @IBOutlet weak var PostOutlet: UIButton!
+    @IBOutlet weak var ImageName: UIImageView!
+
+    @IBAction func SliderValue(_ sender: UISlider) {
+        let SliderValue = Int(Slider.value)
+        SliderLabel.text = "\(SliderValue)"
+    }
+    
+    //image picking//
+    @IBAction func ImageSelect(_ sender: UIButton) {
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.ImageName.image = image
+            PostOutlet.isHidden = false
+        }
+        self.dismiss(animated: false, completion: nil)
+    }
+    //=-=-=-=-=-=-=-=-=-=-=-//
+    
+    @IBAction func Post(_ sender: UIButton)
+        {
+            if (ItemName.text != "" && ItemPrice.text != "" && ImageName.image != nil)
+            {
+                let imageRef = self.StorageRef.child("\(FIRAuth.auth()?.currentUser?.uid).jpg")
+                let data = UIImageJPEGRepresentation(self.ImageName.image!, 1)
+                let upload = imageRef.put(data!, metadata: nil, completion: { (metadata, error) in
+                    if (error != nil)
+                        
+                    {
+                        print(error!.localizedDescription)
+                    }
+                    imageRef.downloadURL(completion: { (url, error2) in
+                        if (error2 != nil)
+                        {
+                            print(error!.localizedDescription)
+                        }
+                        if let url = url {
+                            
+                            let user = FIRAuth.auth()?.currentUser
+                            let email = user?.email
+                            
+                            let postInfo : [String : Any] = ["uid": user!.uid,
+                                                             "item name" : self.ItemName.text!,
+                                                             "price" : self.ItemPrice.text!,
+                                                             "condition" : Int(self.Slider.value),
+                                                             "image" : url.absoluteString]
+                            self.ref.child("posts").child(user!.uid).setValue(postInfo)
+                        }
+                    
+                    })
+                    
+                })
+                upload.resume()
+            }
+            else
+            {
+                Label.text = "Please Fill out all Fields"
+            }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        
+        let Storage = FIRStorage.storage().reference(forURL: "gs://real-fbla-project.appspot.com")
+        ref = FIRDatabase.database().reference()
+        StorageRef = Storage.child("posts")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
